56fc1658b141e8f67df9b729339722003070725d
wangcong <2424772433@qq.com>
2021-10-24 15:50:29 +0800

    表单静态完成

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 import './login.css'
+import logo from './images/LOGO.png'
+import { Form, Icon, Input, Button,} from 'antd';  //引入antd
 
 export default class Login extends Component {
     render() {
         return (
             <div className="login">
-                <header className="login-header"></header>
-                <section className="login-content"></section>
+                <header className="login-header">
+                    <img src={logo} alt="" />
+                    <h1>矢易后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登陆</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="请输入用户名"
+                                />
+                        </Form.Item>
+                        <Form.Item>
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="请输入密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登陆
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
             </div>
         )
     }
