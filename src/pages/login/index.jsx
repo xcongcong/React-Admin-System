bb4cff4a914b365b85ffae1226f39a7610bed0bf
wangcong <2424772433@qq.com>
2021-10-24 13:39:01 +0800

    1024提交

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import './login.css'
 
 export default class Login extends Component {
     render() {
         return (
-            <h1>这里是登陆路由</h1>
+            <div className="login">
+                <header className="login-header"></header>
+                <section className="login-content"></section>
+            </div>
         )
     }
 }
